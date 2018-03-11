06e0d349302652495f4de20f36e13b81bf06dab3
randypx <sinrandy@hotmail.com>
2018-03-11 23:09:15 +0800

    Fixed checkstyle error (+1 squashed commits)
    Squashed commits:
    
    [3e8c142] Fixed checkstyle errors

--- src/test/java/seedu/address/testutil/StudentBuilder.java
+++ src/test/java/seedu/address/testutil/StudentBuilder.java
@@ -1,53 +1,95 @@
 package seedu.address.testutil;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Student;
+import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
-public class StudentBuilder extends PersonBuilder {
+/**
+ * A utility class to help with building Student objects.
+ */
+public class StudentBuilder {
+
+    private static final String DEFAULT_NAME = "Alice Pauline";
+    private static final String DEFAULT_PHONE = "85355255";
+    private static final String DEFAULT_EMAIL = "alice@gmail.com";
+    private static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    private static final String DEFAULT_TAGS = "friends";
+
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Address address;
+    private Set<Tag> tags;
 
     public StudentBuilder() {
-        super();
+        name = new Name(DEFAULT_NAME);
+        phone = new Phone(DEFAULT_PHONE);
+        email = new Email(DEFAULT_EMAIL);
+        address = new Address(DEFAULT_ADDRESS);
+        tags = SampleDataUtil.getTagSet(DEFAULT_TAGS);
     }
 
+    /**
+     * Initializes the StudentBuilder with the data of {@code personToCopy}.
+     */
     public StudentBuilder(Student personToCopy) {
-        super(personToCopy);
+        name = personToCopy.getName();
+        phone = personToCopy.getPhone();
+        email = personToCopy.getEmail();
+        address = personToCopy.getAddress();
+        tags = new HashSet<>(personToCopy.getTags());
     }
 
-    @Override
+    /**
+     * Sets the {@code Name} of the {@code Person} that we are building.
+     */
     public StudentBuilder withName(String name) {
         this.name = new Name(name);
         return this;
     }
 
-    @Override
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
+     */
     public StudentBuilder withTags(String ... tags) {
         this.tags = SampleDataUtil.getTagSet(tags);
         return this;
     }
 
-    @Override
+    /**
+     * Sets the {@code Address} of the {@code Person} that we are building.
+     */
+
     public StudentBuilder withAddress(String address) {
         this.address = new Address(address);
         return this;
     }
 
-    @Override
+    /**
+     * Sets the {@code Phone} of the {@code Person} that we are building.
+     */
+
     public StudentBuilder withPhone(String phone) {
         this.phone = new Phone(phone);
         return this;
     }
 
-    @Override
+    /**
+     * Sets the {@code Email} of the {@code Person} that we are building.
+     */
+
     public StudentBuilder withEmail(String email) {
         this.email = new Email(email);
         return this;
     }
 
-    @Override
     public Student build() {
         return new Student(name, phone, email, address, tags);
     }
