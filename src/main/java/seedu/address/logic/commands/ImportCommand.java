ea7245eb70d1039344a99cc390a341687d8ff6ed
shanmu9898 <shanmu9898@icloud.com>
2018-03-15 23:53:04 +0800

    Checkstyle errors 2

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -1,67 +1,41 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PATH;
-
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Optional;
-import java.util.logging.Logger;
-
 import javafx.collections.ObservableList;
-import seedu.address.MainApp;
-import seedu.address.commons.core.Config;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
-import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.storage.AddressBookStorage;
-import seedu.address.storage.JsonUserPrefsStorage;
 import seedu.address.storage.Storage;
-import seedu.address.storage.StorageManager;
-import seedu.address.storage.UserPrefsStorage;
 import seedu.address.storage.XmlAddressBookStorage;
 
 /**
- * Imports contacts from a different Addressbook file
+ * Imports contacts from a different TeachConnect XML file
  */
 public class ImportCommand extends UndoableCommand {
 
-    protected static final String MESSAGE_INVALID_FILE = "Please input a valid file location";
-    protected Storage storage;
-    protected Config config;
-    protected UserPrefs userPrefs;
-
-
     public static final String COMMAND_WORD = "import";
-
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": imports contacts to the address book."
             + "Parameters: file location...\n"
             + "Example: " + COMMAND_WORD + " main/src/test/data/sandbox/somerandomfile.xml";
-
     public static final String MESSAGE_SUCCESS = "Contacts have been successfully imported!";
-
+    protected static final String MESSAGE_INVALID_FILE = "Please input a valid file location";
+    protected Storage storage;
     private AddressBook addressBookImported;
     private AddressBookStorage addressBookStorage;
-
-
     private String filePath;
 
-
-    private static final Logger logger = LogsCenter.getLogger(ImportCommand.class);
-
+    /**
+     * Creates an ImportCommand to import the specified TeachConnect XML file
+     */
     public ImportCommand(String importPath) {
         requireNonNull(importPath);
         this.filePath = importPath;
         addressBookStorage = new XmlAddressBookStorage(filePath);
-
     }
 
     @Override
@@ -74,12 +48,11 @@ public class ImportCommand extends UndoableCommand {
                     try {
                         model.addPerson(people.get(i));
                     } catch (DuplicatePersonException e) {
-
+                        throw new CommandException(MESSAGE_DUPLICATE_PERSON);
                     }
                 }
-            }else{
+            } else {
                 throw new CommandException(String.format(MESSAGE_INVALID_FILE));
-
             }
         } catch (DataConversionException e) {
             throw new CommandException(String.format(MESSAGE_INVALID_FILE));
--- src/main/java/seedu/address/logic/parser/ImportCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ImportCommandParser.java
@@ -2,15 +2,19 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PATH;
-
-import java.util.Arrays;
-
 import seedu.address.logic.commands.ImportCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**@@author Bumblebee
+ * Parses input arguments and creates a new ImportCommand object
+ */
 public class ImportCommandParser implements Parser<ImportCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the ImportCommand
+     * and returns an ImportCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public ImportCommand parse(String args) throws ParseException {
         requireNonNull(args);
         String trimmedArgs = args.trim();
