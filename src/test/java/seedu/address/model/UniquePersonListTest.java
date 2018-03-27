24256f1fb745882152a06b2482229c0392d1ac84
shanmu9898 <shanmu9898@icloud.com>
2018-03-27 23:06:39 +0800

    Checkstyle Corrections

--- src/test/java/seedu/address/model/UniquePersonListTest.java
+++ src/test/java/seedu/address/model/UniquePersonListTest.java
@@ -1,17 +1,10 @@
 package seedu.address.model;
 
-import static junit.framework.TestCase.assertTrue;
-
-import java.util.List;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.testutil.PersonBuilder;
 
 public class UniquePersonListTest {
     @Rule
@@ -24,19 +17,4 @@ public class UniquePersonListTest {
         uniquePersonList.asObservableList().remove(0);
     }
 
-//    @Test
-//    public void sortedObservableList_modifyList_showsSortedList(){
-//        UniquePersonList uniquePersonList = new UniquePersonList();
-//        Person alice = new PersonBuilder().withName("Alice").build();
-//        Person bob = new PersonBuilder().withName("Bob").build();
-//        try {
-//            uniquePersonList.add(bob);
-//            uniquePersonList.add(alice);
-//        } catch (DuplicatePersonException dpe) {
-//            assert false : "Should not happen";
-//        }
-//        uniquePersonList.sort();
-//        List testingList = uniquePersonList.asObservableList();
-//        assertTrue(testingList.get(0).equals(alice));
-//    }
 }
