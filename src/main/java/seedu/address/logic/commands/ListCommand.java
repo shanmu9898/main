4a679e408b21a733c42aa5d4e9ebfd5a32a41214
randypx <sinrandy@hotmail.com>
2018-03-28 00:52:12 +0800

    updated list command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ONLY_STUDENTS;
+
+import seedu.address.logic.commands.exceptions.CommandException;
 
 /**
  * Lists all persons in the address book to the user.
@@ -9,12 +12,30 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all ";
+    private static final String MESSAGE_INVALID_TYPE = "TYPE is missing or invalid";
 
+    private static final String TYPE_STUDENT = "students";
+    private static final String TYPE_CONTACT = "contacts";
+    private final String typeToListed;
+
+    public ListCommand(String typeArg){
+        typeToListed = typeArg.trim();
+    }
 
     @Override
-    public CommandResult execute() {
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+    public CommandResult execute() throws CommandException{
+        switch (typeToListed) {
+            case TYPE_CONTACT:
+                model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+                return new CommandResult(MESSAGE_SUCCESS + TYPE_CONTACT);
+
+            case TYPE_STUDENT:
+                model.updateFilteredPersonList(PREDICATE_SHOW_ONLY_STUDENTS);
+                return new CommandResult(MESSAGE_SUCCESS + TYPE_STUDENT);
+
+            default:
+                throw new CommandException(MESSAGE_INVALID_TYPE);
+        }
     }
 }
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -69,7 +69,7 @@ public class AddressBookParser {
             return new FindCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
-            return new ListCommand();
+            return new ListCommand(arguments);
 
         case HistoryCommand.COMMAND_WORD:
             return new HistoryCommand();
