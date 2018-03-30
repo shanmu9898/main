18bc46d8234d5a8d6e4d8ed8a8414514570fa843
randypx <sinrandy@hotmail.com>
2018-03-31 05:43:52 +0800

    fixed checkstyle

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -15,12 +15,12 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all ";
-    private static final String MESSAGE_INVALID_TYPE = "TYPE is missing or invalid";
 
     public static final String TYPE_CONTACT = "contact";
     public static final String TYPE_STUDENT = "student";
     public static final String TYPE_APPOINTMENT = "appointment";
     public static final String TYPE_TASK = "task";
+    private static final String MESSAGE_INVALID_TYPE = "TYPE is missing or invalid";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists selected type. \n"
             + "Parameter: TYPE\n"
