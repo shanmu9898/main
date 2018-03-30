d16f853ad0853c88200c69e0c4ab7f329af4e9ee
randypx <sinrandy@hotmail.com>
2018-03-31 05:18:25 +0800

    fixed checkstyle

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -380,7 +380,6 @@ public class AddressBook implements ReadOnlyAddressBook {
 
         updateStudent(student, updatedStudent);
     }
-    
     //@@author Sisyphus25
     //event operations
     /**
