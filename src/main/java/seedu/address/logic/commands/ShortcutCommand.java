eb19ab82db4bebc10fb5bbecdcf358ed43ef978d
Mukesh Gadupudi <e0072523@u.nus.edu>
2018-04-15 15:14:55 +0800

    Update ShortcutCommand.java

--- src/main/java/seedu/address/logic/commands/ShortcutCommand.java
+++ src/main/java/seedu/address/logic/commands/ShortcutCommand.java
@@ -30,7 +30,6 @@ public class ShortcutCommand extends UndoableCommand {
 
     private final String[] commandsPresent = {"add", "clear", "theme", "delete", "edit", "exit", "export", "find",
                                               "help", "history", "import", "list", "redo", "undo", "select",
-
                                               "set_appointment", "set_task", "shortcut", "sort", "undo", "calendar",
                                               "delete_shortcut", "remove", "form"};
 
